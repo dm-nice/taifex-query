682a0c1e8b666e9865bb8558f6da82b8a0bacf0f
DM <dm.nice@gmail.com>
2026-01-15 16:58:00 +0800

    更新

--- utils/debug_pipeline.py
+++
@@ -1,8 +0,0 @@
-import traceback
-
-def format_traceback(e: Exception) -> str:
-    """
-    將例外物件轉換為完整文字錯誤訊息（含 Traceback）
-    用於 run.py 將錯誤直接寫入 log 檔案
-    """
-    return "".join(traceback.format_exception(type(e), e, e.__traceback__))
